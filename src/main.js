d12a653d0046594062bcff8acf9bac4c39443542
txj-xyz <jfinelli3@gmail.com>
2022-01-24 14:55:15 -0700

    dev: hide `MenuBar` and disable `resizing`

--- src/main.js
+++ src/main.js
@@ -1,18 +1,23 @@
+// const { GlobalKeyboardListener } = require('node-global-key-listener')
 const { app, BrowserWindow } = require('electron')
-const path = require('path')
 
 function createWindow () {
+  // const v = new GlobalKeyboardListener()
+  // v.addListener(function (e, down) {
+  //   console.log(e) TODO: Filter keys and start adding buttons and UI elements before starting key binds
+  // });
   const win = new BrowserWindow({
+    autoHideMenuBar: true,
+    resizable: false,
     width: 300,
     height: 200
   })
-
   win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
   createWindow()
-
+  
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
