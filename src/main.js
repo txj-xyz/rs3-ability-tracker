05def1eb68e42352a6df505a73e237c3466d1064
0x0b6e48 <7461476+txj-xyz@users.noreply.github.com>
2022-05-20 10:27:27 -0600

    shitty template binding code

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 // const { GlobalKeyboardListener } = require('node-global-key-listener')
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, globalShortcut } = require('electron')
 const prompt = require('custom-electron-prompt')
 
 function createWindow () {
-  // const win = new BrowserWindow({
-  //   autoHideMenuBar: true,
-  //   resizable: false,
-  //   width: 300,
-  //   height: 200
-  // })
+  const win = new BrowserWindow({
+    autoHideMenuBar: true,
+    resizable: false,
+    width: 300,
+    height: 200
+  })
+  win.loadFile('index.html')
   const kb = ($value, $label, $default) => { return { value: $value, label: $label, default: $default } };
     prompt({
       title: "Keybinds",
@@ -16,22 +17,26 @@ function createWindow () {
       type: "keybind",
       value: "2", // Doesn't do anything here
       keybindOptions: [
-        kb("volumeDown", "Primary bar slot 1", ""),
-        kb("volumeDown", "Primary bar slot 2", ""),
-        kb("volumeDown", "Primary bar slot 3", ""),
-        kb("volumeDown", "Primary bar slot 4", ""),
-        kb("volumeDown", "Primary bar slot 5", ""),
-        kb("volumeDown", "Primary bar slot 6", ""),
+        kb("slot1", "Primary bar slot 1", ""),
+        kb("slot2", "Primary bar slot 2", ""),
+        kb("slot3", "Primary bar slot 3", ""),
+        kb("slot4", "Primary bar slot 4", ""),
+        kb("slot5", "Primary bar slot 5", ""),
+        kb("slot6", "Primary bar slot 6", ""),
       ],
       resizable: false,
     }, app).then(input => {
       if (input)
-        input.forEach(obj => console.log(obj))
-      else
+        input.forEach(obj => {
+          if(obj.accelerator == '') return;
+          console.log(obj)
+          const ret = globalShortcut.register(obj.accelerator, () => {})
+          if(!ret) return console.log('failed to register keybind')
+        })
+        console.log(input)
         console.log("Pressed Cancel");
     })
    .catch(console.error)
-  // win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
