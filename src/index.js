536dc91c46da1a23eeba8ebb04ed560912697923
0x0b6e48 <7461476+txj-xyz@users.noreply.github.com>
2022-06-08 14:12:05 -0600

    give support to windows installer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const { BrowserWindow, app } = require('electron');
 const { resolve } = require('path');
 const manager = require(resolve(__dirname, 'manager.js'));
 
+// If the app is being started from `windows.squirrel` installer then stop and return
+if (require('electron-squirrel-startup')) {
+    app.quit()
+    process.exit(1)
+};
 
 // Make globally usable variables.
 for (const property in manager) global[property] = manager[property];
