40194ac762aee24dbbfd9311d58f9b3576929da1
0x0b6e48 <7461476+txj-xyz@users.noreply.github.com>
2022-06-15 13:21:26 -0600

    recursive read all directories and map

--- test.js
+++ test.js
@@ -1,30 +1,28 @@
-// const { uIOhook } = require('uiohook-napi');
-// uIOhook.start();
-// let arrTemp = [];
-// let modifiers = ['altKey', 'ctrlKey', 'shiftKey', 'metaKey'];
-
-// uIOhook.on('keydown', trigger => {
-//     if(!trigger.includes())
-//     console.log(Date.now(), trigger)
-// });
-
-const { readdirSync } = require('fs')
+const { readdirSync, statSync } = require('fs')
+const { join } = require('path')
 const path = './ability-window/game-asset-groups/'
 const dir = readdirSync(path,  { withFileTypes: true })
 const filesMap = new Map();
 
-//parse all ability icons and put them in a map
-for (const directory of dir) {
-    if (!directory.isDirectory()) continue;
-    for(const file of readdirSync(path + directory.name, { withFileTypes: true })) {
-        if(!file.isFile()) continue;
-        console.log(path + directory.name + '/' + file.name)
-        filesMap.set(file.name.replace(/\.png/g, ''), {
-            name: file.name,
-            type: directory.name,
-            path: path + directory.name,
-        })
-    }
-}
 
-console.log(filesMap)
+const walkSync = (dir, callback) => {
+    const files = readdirSync(dir);
+    files.forEach((file) => {
+      var filepath = join(dir, file);
+      var dirname = join(dir).split(/\\/g).pop();
+      const stats = statSync(filepath);
+      if (stats.isDirectory()) {
+        walkSync(filepath, callback);
+      } else if (stats.isFile()) {
+        callback(filepath, dirname);
+      }
+    });
+  };
+
+walkSync(path, (filepath, dirname) => {
+    console.log({
+        fileName: filepath.replace(/.png/g, '').split(/\\/g).pop(),
+        pathName: filepath,
+        dirName: dirname,
+    })
+})
