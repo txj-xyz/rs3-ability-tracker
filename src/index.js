cd2cdcb3b9c3f896d04e083e77e8d5644c0b0d80
Kurasad <pulavarthy.dhanush@gmail.com>
2022-06-16 19:34:30 -0500

    index rework

--- src/index.js
+++ src/index.js
@@ -1,37 +1,29 @@
 // Import dependencies.
-const { BrowserWindow, app } = require('electron');
-const { resolve } = require('path');
-const manager = require(resolve(__dirname, 'manager.js'));
-const { uIOhook } = require('uiohook-napi');
-app.disableHardwareAcceleration();
-
-// If the app is being started from `windows.squirrel` installer then stop and return
-if (require('electron-squirrel-startup')) {
-    app.quit();
-    process.exit(1);
-}
-
+const [{ BrowserWindow, app }, { resolve }, { uIOhook }] = ['electron', 'path', 'uiohook-napi'].map(require);
 
 // Make globally usable variables.
-for (const property in manager) global[property] = manager[property];
-
+for (const property in (manager = require(resolve(__dirname, './base/Manager.js')).load())) global[property] = manager[property];
 
 app
     // App ready event.
     .on('ready', main)
 
+    // App second instance event.
+    .on('second-instance', main)
+
     // App start event (Run if app is not ready).
     .on('activate', _ => (!BrowserWindow.getAllWindows().length ? app.emit('ready') : void 0))
-    
-    //on second instance load main up
-    .on('second-instance', main)
 
     // App quit event.
     .on('window-all-closed', _ => {
         uIOhook.stop();
         app.isQuiting = true;
         app.quit();
-    });
+        process.exit(1);
+    })
 
-// dont allow second instance.
-!app.requestSingleInstanceLock() ? app.emit('window-all-closed') : void 0;
+    // Disable app hardware acceleration.
+    .disableHardwareAcceleration();
+
+// Do not allow second instances and do not allow the app to be started from 'windows.squirrel'.
+!app.requestSingleInstanceLock() || require('electron-squirrel-startup') ? app.emit('window-all-closed') : void 0;
