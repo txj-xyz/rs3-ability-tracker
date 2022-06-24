710285e78f1e98d4cfa3f19d9871f8d5657e335b
Kurasad <pulavarthy.dhanush@gmail.com>
2022-06-24 17:25:10 -0500

    window startup checks

--- src/pages/MainWindow.js
+++ src/pages/MainWindow.js
@@ -3,11 +3,13 @@ const Window = require('../base/Window.js')
 module.exports = class Main extends Window {
     constructor() {
         super()
-            .create({ ...windows.properties, width: 700, height: 190 }, true)
-            .ipcLoader('mainEmit', this.mainEmit)
+            .create({ ...windows.properties, width: 700, height: 190 })
+            .ipcLoader('mainListener', this.mainListener)
             .ipcLoader('confListener', this.confListener)
+        if (!config.referenceStorage.keybinds.length) new Keybinds()
+        else windows.main.on('ready-to-show', _ => new Main())
     }
 
-    mainEmit = _ => null
+    mainListener = _ => null
     confListener = _ => null
 }
