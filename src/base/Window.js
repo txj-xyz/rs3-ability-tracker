53254776d3ef372d50c99a168c5232e0daa7833a
Kurasad <pulavarthy.dhanush@gmail.com>
2022-06-24 17:24:41 -0500

    fix show logic

--- src/base/Window.js
+++ src/base/Window.js
@@ -10,13 +10,14 @@ module.exports = class Window {
         if (windows[this.name]) {
             windows[this.name].show();
             windows[this.name].focus();
+            // windows.tray.reload();
             this.exists = true
         }
         return this
     }
 
     create(param, show) {
-        if (this.exists) return
+        if (this.exists) return this
         windows[this.name] = new BrowserWindow(param)
         windows[this.name].loadFile(page(this.name))
         windows[this.name].removeMenu()
@@ -30,29 +31,19 @@ module.exports = class Window {
     }
 
     ipcLoader(event, callback) {
-        if (this.exists) return
+        if (this.exists) return this
         this.listeners[event] = callback
         ipcMain.on(event, callback)
         return this
     }
 
     #register(show) {
-        if (show) {
-            windows[this.name].on('ready-to-show', _ => {
-                windows[this.name].show();
-                windows[this.name].focus();
-                // windows.tray.reload();
-            })
-        }
+        if (show) windows[this.name].on('ready-to-show', _ => new global[this.constructor.name]())
         windows[this.name].on('close', _ => {
             this.#emit('closed')
             for (const event in this.listeners) ipcMain.removeListener(event, this.listeners[event])
-            switch (this.name) {
-                case 'main': {
-                    app.emit('window-all-closed');
-                    break;
-                }
-            }
+            if (this.name === 'main') app.emit('window-all-closed')
+            else new Main()
             delete windows[this.name]
         })
     }
