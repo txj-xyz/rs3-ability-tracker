daf70fd8bbe453d4e682da74d03d8d164960c5ce
Kurasad <pulavarthy.dhanush@gmail.com>
2022-06-24 00:21:58 -0500

    config class and updater

--- src/modules/configLoader.js
+++ src/modules/configLoader.js
@@ -1,9 +1,17 @@
 // Import dependencies.
-const [Manager, { resolve }] = ['../base/Manager.js', 'path'].map(require);
+const [Manager, { resolve }, { app }, { writeFileSync }] = ['../base/Manager.js', 'path', 'electron', 'fs'].map(require);
 
 // Check if app is running in a dev environment.
 module.exports = class Config extends Manager {
     static init() {
-        return super.file(resolve(__dirname, `../cfg/game-key-data.json`))
+        const path = resolve(process.argv[2] === 'dev' ? '' : app.getPath('userData'), 'config.json')
+        const config = super.file(path)
+        const handler = {
+            set(data, prop, receiver) {
+                Reflect.set(...arguments);
+                return writeFileSync(path, JSON.stringify(config, null, 2))
+            },
+        };
+        return new Proxy(config, handler)
     }
 };
