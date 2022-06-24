5849f13cbbba78a7c074b1f461f9ef9a199fffba
Kurasad <pulavarthy.dhanush@gmail.com>
2022-06-24 00:22:34 -0500

    file renaming

--- src/modules/PageLoadBuilder.js
+++ src/modules/PageLoadBuilder.js
@@ -2,7 +2,7 @@
 const [Manager, { resolve }] = ['../base/Manager.js', 'path'].map(require);
 
 // Function to get frontend page paths.
-module.exports = class PageLoader extends Manager {
+module.exports = class Page extends Manager {
     static init() {
         return name => resolve(__dirname, `../../ability-window/html/${name}.html`);
     }
