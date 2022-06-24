99377ef860d59131ba5094938476e1ccde44de5b
Kurasad <pulavarthy.dhanush@gmail.com>
2022-06-24 09:51:15 -0500

    add IPC events (blank)

--- src/pages/MainWindow.js
+++ src/pages/MainWindow.js
@@ -1,10 +1,22 @@
+const { ipcMain } = require('electron')
 const Window = require('../base/Window.js')
 
 module.exports = class Main extends Window {
     constructor() {
         super()
         super.give(this)
-        if (!super.instanceCheck()) super.create({ ...windows.properties, width: 700, height: 190, show: true })
+        if (!super.instanceCheck()) super.create({ ...windows.properties, width: 700, height: 190 }, true)
         new Taskbar()
+        this.#registerIPC()
+    }
+
+    #registerIPC() {
+        ipcMain.on('mainEmit', (event, param) => {
+            
+        })
+
+        ipcMain.on('confListener', (event, param) => {
+            
+        })
     }
 }
