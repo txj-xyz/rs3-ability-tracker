8c36816a2ff46aab66a6cbb44a0916b9d1486c2a
Kurasad <pulavarthy.dhanush@gmail.com>
2022-06-24 19:07:32 -0500

    modify for testing

--- src/pages/MainWindow.js
+++ src/pages/MainWindow.js
@@ -3,11 +3,11 @@ const Window = require('../base/Window.js')
 module.exports = class Main extends Window {
     constructor() {
         super()
-            .create({ ...windows.properties, width: 700, height: 190 })
+            .create({ ...windows.properties, width: 700, height: 190 }, true)
             .ipcLoader('mainListener', this.mainListener)
             .ipcLoader('confListener', this.confListener)
-        if (!config.referenceStorage.keybinds.length) new Keybinds()
-        else windows.main.on('ready-to-show', _ => new Main())
+        // if (!config.referenceStorage.keybinds.length) new Keybinds()
+        // else windows.main.on('ready-to-show', _ => new Main())
     }
 
     mainListener = _ => null
