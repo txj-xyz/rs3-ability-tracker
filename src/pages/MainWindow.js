279e94470115525c5f90410ce6ba6e0651e41d3c
txj-xyz <jfinelli3@gmail.com>
2022-06-24 22:13:26 -0600

    event listeners for config and window

--- src/pages/MainWindow.js
+++ src/pages/MainWindow.js
@@ -1,15 +1,40 @@
-const Window = require('../base/Window.js')
+const Window = require('../base/Window.js');
 
 module.exports = class Main extends Window {
     constructor() {
         super()
             .create({ ...windows.properties, width: 700, height: 190 }, true)
-            .ipcLoader('mainListener', this.mainListener)
-            .ipcLoader('confListener', this.confListener)
+            .ipcLoader(this.mainListener, this.confListener)
         // if (!config.referenceStorage.keybinds.length) new Keybinds()
         // else windows.main.on('ready-to-show', _ => new Main())
     }
 
-    mainListener = _ => null
-    confListener = _ => null
+    mainListener = (event, param) => {
+        switch (param) {
+            case 'quit': { quitHandler(); break; }
+        }
+        return event.returnValue = null;
+    }
+
+    confListener = (event, param) => {
+        if(typeof config[param] === 'boolean') {
+            config[param] = !config[param]
+        }
+        else {}
+
+        switch (param) {
+            case 'trackCooldowns': {
+                // if (!config.trackCooldowns) unregisterCooldowns(); Finish on UnregisterManager creation
+                break;
+            }
+            case 'lockTrackerWindow': {
+                // windows.ability?.setMovable(!config.lockTrackerWindow);
+                // windows.ability?.setResizable(!config.lockTrackerWindow);
+                break;
+            }
+        
+            default:
+                break;
+        }
+    }
 }
