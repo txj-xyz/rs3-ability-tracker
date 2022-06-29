131e202472ea2e0d882704abb3d2d2d92746ee73
Kurasad <pulavarthy.dhanush@gmail.com>
2022-06-29 15:50:25 -0500

    main window backend

--- ability-window/javascript/main.js
+++ ability-window/javascript/main.js
@@ -1,29 +1,44 @@
 for (const property in config) {
     const element = document.getElementById(property)
-    if (element) element.getAttribute('type') === 'checkbox' ? element.checked = config[property] : element.value = config[property]
-    if (element && element.id === 'lockTrackerWindow') {
-        lockTrackerWindowToggle()
-        element.addEventListener('change', lockTrackerWindowToggle)
+    if (element) {
+        element.getAttribute('type') === 'checkbox' ? element.checked = config[property] : element.value = config[property]
+        switch (element.id) {
+            case 'lockTrackerWindow': {
+                lockTrackerWindowToggle()
+                element.addEventListener('change', lockTrackerWindowToggle)
+                break
+            }
+
+            case 'barsSelection': {
+                new Dropdown(element.parentElement, config.referenceStorage.bars)
+                break
+            }
+        }
     }
+
 }
 
-if (!config.referenceStorage.bars.length) document.querySelector('label.bars').classList.add('disabled')
+if (!config.referenceStorage.bars.length) document.querySelector('label[bars]').classList.add('disabled')
 
 document.querySelectorAll('input[id]').forEach(input => {
-    // if (input.id === 'lockTrackerWindow') lockTrackerWindowToggle()
-    input.onclick = _ => request('confListener', { id: input.id, value: input.value })
+    input.onclick = _ => {
+        if (input.id === 'lockTrackerWindow') lockTrackerWindowToggle()
+        request('confListener', { id: input.id, value: input.value })
+    }
 })
-document.querySelectorAll('div.buttons > div').forEach(button => button.onclick = _ => request('mainListener', button.id))
+document.querySelectorAll('div[buttons] > div').forEach(button => button.onclick = _ => request('mainListener', button.id))
 
 const slider = document.getElementById('numberOfIcons')
 slider.onclick = null
 slider.addEventListener('input', _ => {
-    document.getElementById('label').innerHTML = slider.value;
+    document.querySelector('b[label]').innerHTML = slider.value;
     request('confListener', { id: slider.id, value: parseInt(slider.value) })
 });
 
-const selection = document.getElementById('barsSelection')
-
 function lockTrackerWindowToggle() {
-    document.querySelector('label.lock > span').innerHTML = document.getElementById('lockTrackerWindow').checked ? '🔒' : '🔓'
+    document.querySelector('label[lock] > span').innerHTML = document.getElementById('lockTrackerWindow').checked ? '🔒' : '🔓'
+}
+
+function update(id, value) {
+    document.querySelector(`div#${id} input`).value = value
 }
