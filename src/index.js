1a41d15bd8bcb028e5486777a12053d883b2797a
Kurasad <pulavarthy.dhanush@gmail.com>
2022-06-30 17:54:41 -0500

    css components

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app
         windows.tray.destroy();
     })
 
-    .on('quit', _ => process.exit(1))
+    .on('quit', process.exit)
 
     // Disable app hardware acceleration.
     .disableHardwareAcceleration();
