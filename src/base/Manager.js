87d1b02c4243ce63397e60b2b140902d9fa669c6
txj-xyz <jfinelli3@gmail.com>
2022-07-03 15:05:26 -0600

    yea

--- src/base/Manager.js
+++ src/base/Manager.js
@@ -3,7 +3,6 @@ const [{ readdirSync, existsSync, writeFileSync }, { resolve }] = ['fs', 'path']
 
 // Load all modules into global.
 module.exports = class Manager {
-
     static windows = {
         properties: {
             icon: resolve(__dirname, '../icons/icon.png'),
@@ -17,40 +16,71 @@ module.exports = class Manager {
                 contextIsolation: false,
             },
         },
-    }
+    };
 
     // Loading logic.
     static load() {
-        this.once = this.once ? true : false
+        this.once = this.once ? true : false;
         if (!this.once) {
             ['modules', 'pages']?.map(dir => {
                 readdirSync(resolve(__dirname, `../${dir}`)).map(file => {
                     const Module = require(resolve(__dirname, `../${dir}/${file}`));
-                    const globalVar = Module.name?.startsWith('_')
+                    const globalVar = Module.name?.startsWith('_');
                     const name = (globalVar ? '_' : '') + (Module.init ? Module.name.slice(0, globalVar ? 2 : 1).toLowerCase() + Module.name.slice(globalVar ? 2 : 1) : Module.name);
                     this[name] = Module.init ? Module.init() : Module;
                 });
             });
-            this.once = true
+            this.once = true;
         }
         return this;
     }
 
+    static checkConfig(oldConfig, newConfig) {
+        let oldProps = Object.keys(oldConfig).filter(key => !['referenceStorage', 'abilityWindow'].includes(key));
+        let newProps = Object.keys(newConfig).filter(key => !['referenceStorage', 'abilityWindow'].includes(key));
+        let oldDiff = oldProps.filter(key => !newProps.includes(key)) || [];
+        let keybinds = [];
+        oldDiff.map(key => delete oldConfig[key]);
+        let newDiff = newProps.filter(key => !oldProps.includes(key)) || [];
+        newDiff.map(key => (oldConfig[key] = newConfig[key]));
+
+        if (oldConfig.referenceStorage.keybinds.length && oldConfig.referenceStorage.keybinds[0].key) {
+            oldConfig.referenceStorage.keybinds.map(k => {
+                for (const entry of k.key) {
+                    keybinds.push({ name: k.name.replace(/_/g, ' '), keybind: entry, bar: k.bar, perk: null });
+                }
+            });
+            oldConfig.referenceStorage.keybinds = keybinds;
+
+            oldConfig.abilityWindow.width = 800;
+            oldConfig.abilityWindow.height = 80;
+        }
+
+        return oldConfig;
+    }
+
     // Import file with error catching logic.
     static file(path) {
-        let [failed, data] = [false]
+        let [failed, data] = [false];
+
         // Check if file exists.
         if (existsSync(path)) {
             // Check if file data is corrupted.
             try {
                 data = require(path);
+                if (path.endsWith('config.json')) {
+                    data = this.checkConfig(require(path), require(resolve(__dirname, `../default/${path.replace(/\\/g, '/').split('/').pop()}`)));
+                }
             } catch (e) {
+                console.log(e)
                 failed = true;
             }
 
             // If not corrupted, return data.
             if (!failed) return data;
         }
+
+        
 
         // In any other case, load default data.
         try {
@@ -65,12 +95,12 @@ module.exports = class Manager {
     }
 
     static mapify(data, key) {
-        const map = new Map()
-        if (Array.isArray(data)) data.map(set => map.set(set[key], set))
+        const map = new Map();
+        if (Array.isArray(data)) data.map(set => map.set(set[key], set));
         else {
-            for (const key in data) map.set(key, data[key])
+            for (const key in data) map.set(key, data[key]);
         }
-        return map
+        return map;
     }
 
     // Default game configs.
