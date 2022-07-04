31cf6efc1060d63343bfd643b708b2fe22541f59
txj-xyz <jfinelli3@gmail.com>
2022-07-03 20:15:20 -0600

    disable switching for now

--- src/pages/MainWindow.js
+++ src/pages/MainWindow.js
@@ -3,7 +3,7 @@ const Window = require('../base/Window.js');
 module.exports = class Main extends Window {
     constructor() {
         super()
-            .create({ ...windows.properties, width: 250, height: 358 }, true)
+            .create({ ...windows.properties, width: 250, height: 334 }, true)
             .ipcLoader(this.mainListener, this.confListener)
         windows.update?.focus();
     }
