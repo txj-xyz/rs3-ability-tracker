1de30a7b7f45d6746c96e5c4b217bce762d39437
txj-xyz <jfinelli3@gmail.com>
2022-07-04 00:09:01 -0600

    `__userData` instead of resolve each time

--- src/pages/KeybindsWindow.js
+++ src/pages/KeybindsWindow.js
@@ -1,18 +1,10 @@
-const [{ existsSync, mkdirSync, copyFileSync, unlinkSync }, { resolve }, { dialog }, Window] = ['fs', 'path', 'electron', '../base/Window.js'].map(require)
+const [{ copyFileSync, unlinkSync }, { resolve }, { dialog }, Window] = ['fs', 'path', 'electron', '../base/Window.js'].map(require)
 
 module.exports = class Keybinds extends Window {
     constructor() {
         super()
-            .create({ ...windows.properties, width: 635, height: 344 }, true)
+            .create({ ...windows.properties, width: 635, height: 550 }, true)
             .ipcLoader(this.keybindsListener)
-        this.customPath = this.checkCustomFolder()
-    }
-    checkCustomFolder () {
-        const customPath = resolve(__userData, '.custom');
-        if(!existsSync(customPath)){
-            mkdirSync(customPath)
-        }
-        return customPath;
     }
 
     keybindsListener = (event, param) => {
@@ -24,21 +16,24 @@ module.exports = class Keybinds extends Window {
             }
 
             case 'dialog': {
-                dialog.showOpenDialog({
-                    title: 'Upload a new icon!',
-                    defaultPath: this.customPath,
-                    buttonLabel: 'Upload',
-                    filters: [{ name: 'Images', extensions: ['jpg', 'png', 'gif'] }],
-                    properties: __platform === 'darwin' ? ['openFile', 'createDirectory'] : ['openFile']
-                }).then(file => {
-                    if (!file.canceled) {
-                        const id = randomID()
-                        const path = resolve(this.customPath, `${id}.${file.filePaths[0].toString().split('.').pop()}`)
-                        copyFileSync(file.filePaths[0].toString(), path)
-                        library.set(param.name, path.replace(/\\/g, '/'))
-                        windows.keybinds?.webContents.send('customIcon', { ...library.get(param.name), id: param.id })
-                    }
-                }).catch(console.log)
+                dialog
+                    .showOpenDialog({
+                        title: 'Upload a new icon!',
+                        defaultPath: resolve(__userData, '.custom'),
+                        buttonLabel: 'Upload',
+                        filters: [{ name: 'Images', extensions: ['jpg', 'png', 'gif'] }],
+                        properties: __platform === 'darwin' ? ['openFile', 'createDirectory'] : ['openFile'],
+                    })
+                    .then(file => {
+                        if (!file.canceled) {
+                            const id = randomID();
+                            const path = resolve(resolve(__userData, '.custom'), `${id}.${file.filePaths[0].toString().split('.').pop()}`);
+                            copyFileSync(file.filePaths[0].toString(), path);
+                            library.set(param.name, path.replace(/\\/g, '/'));
+                            windows.keybinds?.webContents.send('customIcon', { ...library.get(param.name), id: param.id });
+                        }
+                    })
+                    .catch(console.log);
                 break;
             }
 
@@ -51,6 +46,10 @@ module.exports = class Keybinds extends Window {
                     }
                 })
                 windows.bars?.webContents.send('fromKeybinds', config.referenceStorage.keybinds)
+                if(windows.ability) {
+                    unregister()
+                    new Trigger()
+                }
                 break;
             }
         }
