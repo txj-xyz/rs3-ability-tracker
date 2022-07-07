dbff674ab017c48d5169ac1abd3bfd9ce639a43b
txj-xyz <jfinelli3@gmail.com>
2022-07-07 00:36:23 -0600

    fix: broken window bounds on windows 10+

--- src/pages/AbilityWindow.js
+++ src/pages/AbilityWindow.js
@@ -1,38 +1,49 @@
-const [{ screen }, Window] = ['electron', '../base/Window.js'].map(require)
+const [{ screen }, Window] = ['electron', '../base/Window.js'].map(require);
 
 module.exports = class Ability extends Window {
     constructor() {
-        super()
-            .create({
-                ...windows.properties,
-                width: config.abilityWindow.width,
-                height: config.abilityWindow.height,
-                x: config.abilityWindow.x ?? (screen.getPrimaryDisplay().workArea.width - config.abilityWindow.width) / 2,
-                y: config.abilityWindow.y ?? (screen.getPrimaryDisplay().workArea.height - config.abilityWindow.height) / 2,
-                frame: false,
-                transparent: true,
-                hasShadow: false,
-                resizable: !config.lockTrackerWindow,
-                alwaysOnTop: true,
-                movable: !config.lockTrackerWindow,
-            }, false)
-        new Confirmation()
-        windows.ability.on('moved', this.updateConfig);
-        windows.ability.on('resize', this.updateConfig);
-        this.updateConfig()
+        super().create({
+            ...windows.properties,
+            width: config.abilityWindow.height * config.numberOfIcons,
+            height: config.abilityWindow.height,
+            minHeight: 50,
+            x: config.abilityWindow.x ?? (screen.getPrimaryDisplay().workArea.width - config.abilityWindow.width) / 2,
+            y: config.abilityWindow.y ?? (screen.getPrimaryDisplay().workArea.height - config.abilityWindow.height) / 2,
+            frame: false,
+            transparent: true,
+            hasShadow: false,
+            resizable: !config.lockTrackerWindow,
+            alwaysOnTop: true,
+            movable: !config.lockTrackerWindow,
+        });
+        new Confirmation();
+        this.fixBounds();
+        windows.ability?.on('moved', this.updatePostition);
+        windows.ability?.on('resized', this.updateSize);
+        windows.ability?.setAspectRatio(config.numberOfIcons);
+        
     }
 
-    updateConfig = _ => {
-        // If the window is moved or resized then update config.
-        if (JSON.stringify(config.abilityWindow) !== JSON.stringify(windows.ability.getBounds())) {
-            config.abilityWindow = windows.ability.getBounds();
-            config.abilityWindow.width = config.abilityWindow.height * config.numberOfIcons;
-
-            // Update aspect ratio to prevent buggy resize.
-                windows.ability?.setResizable(true);
-            windows.ability.setSize(config.abilityWindow.height * config.numberOfIcons, config.abilityWindow.height);
-            windows.ability.setAspectRatio(+config.numberOfIcons.toFixed(2));
-            windows.ability?.setResizable(!config.lockTrackerWindow);
-        }
+    fixBounds = _ => {
+        setTimeout(_ => {
+            if (windows.ability?.getSize()[0] !== config.abilityWindow.height * config.numberOfIcons) {
+                windows.ability.setSize(config.abilityWindow.height * config.numberOfIcons, config.abilityWindow.height);
+            }
+        }, 50);
     };
-}
+
+    updatePostition = _ => {
+        const [x, y] = windows.ability.getPosition();
+        config.abilityWindow.x = x;
+        config.abilityWindow.y = y;
+        this.fixBounds();
+    };
+
+    updateSize = _ => {
+        let [width, height] = windows.ability.getSize();
+        height = height < 50 ? 50 : height;
+        config.abilityWindow.height = height;
+        config.abilityWindow.width = config.numberOfIcons * height;
+        windows.ability.setSize(config.numberOfIcons * height, height);
+    };
+};
