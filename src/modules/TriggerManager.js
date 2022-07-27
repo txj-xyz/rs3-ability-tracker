354db2a5842db2088fc9e8b0a0e7b45a3ceec8ed
txj-xyz <jfinelli3@gmail.com>
2022-07-26 23:28:27 -0600

    feat: working passive style switching / limiting.

--- src/modules/TriggerManager.js
+++ src/modules/TriggerManager.js
@@ -1,4 +1,4 @@
-const { windows } = require( "../base/Manager" );
+const { windows } = require('../base/Manager');
 
 // Import dependencies.
 const [Manager, { uIOhook }, activeWindow] = ['../base/Manager.js', 'uiohook-napi', 'active-win'].map(require);
@@ -51,6 +51,21 @@ module.exports = class Trigger extends Manager {
         return val ? name : '';
     }
 
+    checkRSSwitches({ _falsePositive, bind, _isAbility, reference, _isWeapon, _abilityType }) {
+        let passed = [];
+        passed.push(!_falsePositive)
+        passed.push([this.activeBar, 'Global'].includes(bind.bar))
+        passed.push((_isAbility && this.currentWeapon.style === reference.style) || (_isAbility && bind.bar === 'Global') || _isWeapon || !reference?.style)
+        passed.push((passed[2] && _isAbility ? _abilityType === this.currentWeapon.type : true ))
+
+        __devMode && console.log('passed check?', !passed.includes(false))
+        __devMode && console.log('passed array', passed)
+        __devMode && console.log('ability type:', _abilityType)
+        __devMode && console.log('current weapon', this.currentWeapon)
+        __devMode && console.log('------------------------------')
+        return !passed.includes(false)
+    }
+
     handleKeyPress(trigger) {
         let success = false;
         try {
@@ -59,37 +74,48 @@ module.exports = class Trigger extends Manager {
                 .map(prop => (prop.endsWith('Key') && trigger[prop] ? prop : null))
                 .filter(e => e);
             const key = keycodes.reverseMap.get(trigger.keycode.toString());
-            
+
             //prettier-ignore
             if (Object.keys(modifiers).map(k => modifiers[k]).includes(key)) return
             const possibleKeys = pressedModifiers.some(e => e) ? pressedModifiers.map(mod => `${modifiers[mod]} + ${key}`) : [key];
-            
+
             for (const keybind of possibleKeys) {
                 let binds = config.referenceStorage.keybinds.filter(e => e.keybind === keybind);
                 let globals = binds.filter(e => e.bar === 'Global');
                 binds = binds.filter(e => e.bar !== 'Global');
                 globals.map(e => binds.push(e));
-
                 for (const bind of binds) {
+                    let _falsePositive = false;
                     if (!success) {
                         // anti-spam
                         if (bind.name === this.lastKey.value && Date.now() - this.lastKey.timestamp < rsOptions.spamCooldown) return;
 
                         // GRAB BIND INFORMATION
                         const reference = library.get(bind.name);
-                        const _isWeapon = reference.icon.match(/(weapons\/(magic|melee|range)|slot-icons)/g);
+                        const _abilityType = reference.icon.match(/((?<=abilities\/)(magic|melee|range)|slot-icons)/g)?.[0] ?? null;
+                        const _isWeapon = reference.icon.match(/(weapons\/(magic|melee|range)|slot-icons)/g) ?? null;
+                        // const _isArmor = reference.icon.match(/(armor\/(magic|melee|range)|slot-icons)/g);
+                        const _isAbility = !_isWeapon;
 
                         // SWITCHSCAPE ???
-                        if(reference?.style && _isWeapon) this.currentWeapon.style = reference?.style ?? null;
+                        if (reference.style && _isWeapon) {
+                            this.currentWeapon.style = reference.style ?? null;
+                            this.currentWeapon.name = bind.name ?? null;
+                            this.currentWeapon.type = reference.icon.match(/((?<=weapons\/)(magic|melee|range)|slot-icons)/g)?.[0] ?? null;
+                        }
+
+                        // If no weapon pushed, then only show Global stuff
+                        if (!this.lastKey.value && _isAbility ? bind.bar !== 'Global' : void 0) {
+                            console.log('false positive')
+                            _falsePositive = true;
+                        }
 
                         // swap active bar if toggleSwitching is enabled
                         // if (config.toggleSwitching && reference.icon.match(/(weapons\/(magic|melee|range)|slot-icons)/g) && bind.bar.toLowerCase() !== this.activeBar?.toLowerCase()) {
                         //     this.activeBar = bind.bar;
                         // }
 
-                        // Show bar only binds, including global
-
-                        if ([this.activeBar, 'Global'].includes(bind.bar)) {
+                        if (config.toggleSwitching ? this.checkRSSwitches({ _falsePositive, bind, _isAbility, reference, _isWeapon, _abilityType }) : [this.activeBar, 'Global'].includes(bind.bar) ) {
                             success = true;
                             this.lastKey.value = bind.name;
                             this.lastKey.timestamp = Date.now();
@@ -100,13 +126,15 @@ module.exports = class Trigger extends Manager {
             }
 
             // Handle bar switching here directly for bar keybinds.
-            for(const key of possibleKeys){
+            for (const key of possibleKeys) {
+                console.log(key)
                 let _bind = config.referenceStorage.bars.find(bar => bar.key === key);
-                if(_bind && _bind?.name && !config.toggleSwitching) {
-                    this.activeBar = _bind.name
-                    windows.main?.webContents.send('fromTrigger', this.activeBar)
+                if (_bind && _bind?.name && !config.toggleSwitching) {
+                    this.activeBar = _bind.name;
+                    windows.main?.webContents.send('fromTrigger', this.activeBar);
                 }
             }
+
 
         } catch (error) {
             console.log(error);
